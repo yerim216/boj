1778592f9f7bf4b203df59d7b00632e453efc68e
bing_01 <bing_01@naver.com>
2022-03-03 17:46:25 +0900

    1065

---
+++ 1065/1065.cpp
@@ -0,0 +1,43 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	int N;
+	cin >> N;
+	if (N < 100) {
+		cout << N << endl;
+	}
+	else if(N<1000)
+	{
+		int number =99, cnt = 100;
+
+		while (true)
+		{
+	
+			if (cnt/ 100 - (cnt % 100 / 10) == (cnt % 100 / 10) - (cnt % 100 % 10)) {
+				number++;
+				cnt++;
+			}
+			else 
+			{
+				
+				if (cnt == N) {
+					break;
+				}
+				cnt++;
+				continue;
+			}
+			
+			if (cnt > N) {
+				break;
+			}
+
+			
+		}
+		cout << number << endl;
+
+	}
+	else if (N == 1000) {
+		cout << 144 << endl;
+	}
+}
